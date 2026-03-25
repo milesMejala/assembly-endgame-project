d97c7051334451a27fbfb5e27830254bd9fdec48
milesMejala <emstevenpatrick@gmail.com>
2026-03-25 16:25:26 +0800

    feat: Implement letter guessing functionality with visual feedback and add a new game button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import { languages } from "../language";
 import { useState } from "react";
-
+import { clsx } from "clsx"
+ 
 export default function App() {
   const languageElements = languages.map((language, index) => (
     <span
@@ -24,13 +25,27 @@ export default function App() {
     </span>
   ));
 
+  const [guessLetter, setGuessLetter] = useState([])
+  const chooseLetter = (letter) => {
+    setGuessLetter(prevGuessLetter => 
+      prevGuessLetter.includes(letter) ? prevGuessLetter : [...prevGuessLetter, letter]
+    )
+  }
+
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const alphabetArray = [...alphabet];
+  const alphabetArray = [...alphabet.toUpperCase()];
   const alphabetElements = alphabetArray.map((letter, index) => (
-    <button className="alphabet-letter" key={index}>
+    <button 
+      key={index}
+      className={clsx("alphabet-letter", {
+        "right-letter": guessLetter.includes(letter) && wordArray.includes(letter),
+        "wrong-letter": guessLetter.includes(letter) && !wordArray.includes(letter)
+      })}
+      onClick={() => chooseLetter(letter)}
+    >
       {letter}
     </button>
-  ));
+  ))
 
   return (
     <>
@@ -48,6 +63,7 @@ export default function App() {
         <div className="languages-wrapper">{languageElements}</div>
         <div className="current-word-wrapper">{currentWordElements}</div>
         <section className="keyboard-section">{alphabetElements}</section>
+        <button className="new-game-btn">New Game</button>
       </main>
     </>
   );
