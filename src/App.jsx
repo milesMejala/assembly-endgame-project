9f78f646b3447ed8cd8b4d72157285f10339fcda
milesMejala <emstevenpatrick@gmail.com>
2026-03-25 11:29:14 +0800

    feat: Create initial React app structure with language data display and global styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import "./App.css";
+import { languages } from "../language";
 
 export default function App() {
+  const languageElements = languages.map((language) => 
+    <span className="language-card" style={{backgroundColor: language.backgroundColor, color: language.color}}>{language.name}</span>);
 
   return (
     <>
-      
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <main>
+        <section className="status-section">
+          <p className="status-text">"Farewell HTML & CSS"</p>
+        </section>
+        <div className="languages-wrapper">{languageElements}</div>
+      </main>
     </>
-  )
+  );
 }
-
